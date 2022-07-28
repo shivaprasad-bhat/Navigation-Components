c238899bc01a145e4a91affd9db7356716445ee5
shivaprasad-bhat <shivaprasad@svbneelmane.me>
2022-07-28 17:10:38 +0530

    feat(): Optimized importss

--- app/src/main/java/me/svbneelmane/navigation_components/LoginFragment.kt
+++ app/src/main/java/me/svbneelmane/navigation_components/LoginFragment.kt
@@ -6,12 +6,11 @@ package me.svbneelmane.navigation_components
  * @date-created 27-Jul-22
  */
 import android.app.AlertDialog
-import android.content.DialogInterface
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
 import me.svbneelmane.navigation_components.databinding.FragmentLoginBinding
 
